86c8edd977ff63881896e50fd79d92eca8871777
Hugo <hoyola@ucv.edu.pe>
2024-09-23 17:33:00 -0500

    Add AddCharacterComponent and ListComponent to DbzModule

--- src/app/dbz/page/main-page.component.ts
+++ src/app/dbz/page/main-page.component.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { Character } from '../interfaces/character.interface';
 
 @Component({
   selector: 'app-dbz-main-page',
   templateUrl: 'main-page.component.html',
 })
 export class MainPageComponent {
-  constructor() {}
+  public characters: Character[] = [
+    {
+      name: 'Goku',
+      power: 15000,
+    },
+    {
+      name: 'Vegeta',
+      power: 7500,
+    },
+    {
+      name: 'Gohan',
+      power: 5000,
+    },
+  ];
+
+  onNewCharacter(character: Character): void {
+    // this.characters.push(character);
+    console.log('Main Page Component');
+    console.log(character);
+  }
 }
